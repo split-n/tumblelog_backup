8b16c968ef53ec990e758c805d89b350c1896d8b
ri <git@tnu.jp>
2013-12-23 01:21:04 +0900

    originalのリソースから画像をとるのをやめた
    (画像が降ってくるとは限らないが、チェックをしていないため)

--- lib/post.rb
+++ lib/post.rb
@@ -37,14 +37,12 @@ class Post
   def detect_larger_image_urls
     # 良質なものから順に配列で複数urlを返す
     # urlがあっても取得できない場合があるので
-    source = @data["source_url"]
     photos = @data["photos"][0]
     original_size = photos["original_size"]
     alt_sizes = photos["alt_sizes"]
     alt_maxes = alt_sizes.sort_by{|img| -(img["width"] * img["height"]) }.take(2)
 
     images = []
-    images << source if source
     images << original_size["url"] if original_size
     images.concat(alt_maxes.map{|p| p["url"] } ) 
 
