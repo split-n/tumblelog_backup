8c49a0e50a22608ea6356ce8e90c2f2ac60bb566
ri <git@tnu.jp>
2013-12-22 15:56:05 +0900

    encodingを指定した

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,4 @@
+# encoding:utf-8
 class Post
   def initialize(post_data)
     @data = post_data
--- lib/save_store.rb
+++ lib/save_store.rb
@@ -1,16 +1,20 @@
+# encoding:utf-8
+require 'open-uri'
 class SaveStore
   def initialize(host)
-    @save_dir = ""./#{host}/"
+    @save_dir = File.expand_path(File.dirname($0)) + "/#{host}/"
     FileUtils.mkdir_p("#{@save_dir}json/")
   end
 
-  def save_quote(json)
+  def save_quote(hash)
 
   end
 
-  def save_photo(json)
+  def save_photo(hash)
     dir = "#{@save_dir}image/"
     FileUtils.mkdir_p(dir)
   end
+
+
 
 end
