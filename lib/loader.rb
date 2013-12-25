b71ddb06dfcdc802fc7725b3b25f79e7e63ff803
ri <git@tnu.jp>
2013-12-25 20:11:17 +0900

    自ライブラリの一括ロード

---
+++ lib/loader.rb
@@ -0,0 +1,7 @@
+# encoding:utf-8
+require_relative 'post.rb'
+require_relative 'photo_post.rb'
+require_relative 'post_factory.rb'
+
+require_relative 'save_store.rb'
+require_relative 'tumblelog.rb'
