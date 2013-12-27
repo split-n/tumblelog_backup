2630ec23c14108ebcba5d56e2920cf78a44ebf6c
ri <git@tnu.jp>
2013-12-28 00:18:29 +0900

    とりあえず全クラスのPost派生クラスを作成した
    Photo以外はすべてsaveがjsonを保存するだけの動作

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,6 +1,7 @@
 # encoding:utf-8
 require_relative 'post.rb'
 require_relative 'photo_post.rb'
+require_relative 'misc_post.rb'
 require_relative 'post_factory.rb'
 
 require_relative 'save_store.rb'
--- lib/post_factory.rb
+++ lib/post_factory.rb
@@ -5,10 +5,22 @@ class PostFactory
   def self.create(hash)
     type = hash["type"].to_sym
     return case type
+    when :text
+      TextPost.new(hash)
+    when :quote
+      QuotePost.new(hash)
+    when :link
+      LinkPost.new(hash)
+    when :answer
+      AnswerPost.new(hash)
+    when :video
+      VideoPost.new(hash)
+    when :audio
+      AudioPost.new(hash)
+    when :chat
+      ChatPost.new(hash)
     when :photo
       PhotoPost.new(hash)
-    when :quote
-      uotePost.new(hash)
     else
       nil
     end
