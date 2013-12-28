796d4db6a01f2bdd683baf4443a8e72cde878ce6
ri <git@tnu.jp>
2013-12-28 20:25:56 +0900

    SaveStore#save_photo : 2kb以下でもそれしかない時は保存することにした、保存失敗時に例外を出すようにした

--- lib/save_store.rb
+++ lib/save_store.rb
@@ -1,5 +1,8 @@
 # encoding:utf-8
 require 'open-uri'
+
+class SaveFailedError < RuntimeError ; end
+
 class SaveStore
   def initialize(save_dir)
     @save_dir =  save_dir
@@ -18,19 +21,34 @@ class SaveStore
   end
 
   def save_photo(image_urls,filename_without_extension)
+    min_img = []
     image_data = image_urls.lazy.map {|url|
       result = try_open_url(url)
       next false unless result
-      next false if result.size < 1024*2 # less than 2KB
+      if result.size < 1024*2 # less than 2KB
+        min_img << [url,result]
+        next false
+      end
       next [url,result]
     }.first
+
+    if !image_data
+      min = min_img.shift
+      if min
+        image_data = min
+      else
+        raise SaveFailedError
+      end
+    end
+
+
 
     image = image_data[1]
     extension = File.extname(image_data[0])
 
     File.binwrite("#{@save_dir}photo/#{filename_without_extension}#{extension}",image)
 
-    
+    nil
   end
 
   def try_open_url(url)
