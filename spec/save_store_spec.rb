86e28da1d527aba28f49bc37a925e1f113bd1059
ri <git@tnu.jp>
2013-12-28 22:33:21 +0900

    できる予定の設計のspecを書いた

--- spec/save_store_spec.rb
+++ spec/save_store_spec.rb
@@ -5,59 +5,37 @@ require 'tmpdir'
 Bundler.require
 
 describe SaveStore do
-  it "画像が保存できるのを確認" do
-    Dir.mktmpdir do |tmpdir|
-      img_url = 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/36/13-09-01-kochtreffen-wien-RalfR-02.jpg/800px-13-09-01-kochtreffen-wien-RalfR-02.jpg'
-      target = [img_url]
-      store = SaveStore.new(tmpdir)
-      store.save_photo(target,"test")
-
-      expect_path = "#{tmpdir}photo/test.jpg"
-      expect(File.exist?(expect_path)).to be_true
-
-      expect_data = File.binread('./file/lime.jpg')
-      the_data = File.binread(expect_path)
-
-      expect(expect_data).to eq the_data
+  context "PhotoPostを保存できる" do
+    before :all do
+      @json = File.read('./file/photo_post.json')
+      @data = JSON.load(@json)
+      @photo_post = PhotoPost.new(@data)
+      @tmpdir = Dir.mktmpdir
+      store = SaveStore.new(@tmpdir)
+      store.save(@photo_post)
     end
-  end
 
-  it "2kb以下の画像でも保存できる" do
-    Dir.mktmpdir do |tmpdir|
-      img_url = 'http://k.yimg.jp/images/top/sp2/cmn/logo-ns-130528.png'
-      target = [img_url]
-      store = SaveStore.new(tmpdir)
-      store.save_photo(target,"test")
-
-      expect_path = "#{tmpdir}photo/test.png"
-      expect(File.exist?(expect_path)).to be_true
+    it "画像を保存できている" do
+      expect_photo_path = "#{tmpdir}/photo/json/#{@photo_post.id}.png"
+      stored_photo = File.binread(expect_photo_path)
+      prepared_photo = File.binread('./file/lenna.png')
+      expect(stored_photo).to eq prepared_photo
     end
-  end
 
-  it "保存できない時は例外" do
-    Dir.mktmpdir do |tmpdir|
-      img_url = 'http://www.example.com/abc.jpg'
-      target = [img_url]
-      store = SaveStore.new(tmpdir)
-      expect{
-      store.save_photo(target,"test")
-      }.to raise_error SaveFailedError
+    it "jsonを保存できている" do
+      expect_json_path =  "#{tmpdir}/photo/json/#{@photo_post.id}.json"
+      stored_json_file = File.read(expect_json_path)
+      the_data = JSON.load(stored_json)
+      expect(the_data).to eq @data
     end
-  end
 
-  it "hashをjsonで保存できる、内容が一致する" do
-    Dir.mktmpdir do |tmpdir|
-      target = {"abc" => 123,"def" => "456"}
-      store = SaveStore.new(tmpdir)
-      store.save_json(target,"test")
 
-      expect_path = "#{tmpdir}json/test.json"
-      expect(File.exist?(expect_path)).to be_true
-
-      result = JSON.load(File.read(expect_path))
-      expect(target).to eq result
+    after :all do
+      FileUtils.remove_entry_secure @tmpdir
     end
   end
 
 end
+
+
 
