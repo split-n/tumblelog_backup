438e2830e8416c42ad5331cfdcbe8eb0a12dad97
ri <git@tnu.jp>
2013-12-29 14:20:55 +0900

    add PhotoPost spec

---
+++ spec/photo_post_spec.rb
@@ -0,0 +1,49 @@
+# encoding:utf-8
+require 'bundler'
+require_relative '../lib/loader.rb'
+require 'tmpdir'
+Bundler.require
+
+describe PhotoPost do
+  before :all do
+  end
+
+  let(:data)  {
+    json_file = File.read('./file/photo_post.json')
+    JSON.load(json_file)
+  }
+
+
+  it "インスタンスが生成される" do
+    expect {
+      photo_post = PhotoPost.new(data) 
+    }.not_to raise_error
+  end
+
+  it "適当な内容だとインスタンスが生成されない" do
+    invalid_data = {}
+    expect {
+      PhotoPost.new(invalid_data) 
+    }.to raise_error
+  end
+
+  context "インスタンス生成後" do
+
+    let(:photo_post) {
+      PhotoPost.new(data)
+    }
+
+    it "idが正しい" do
+      expect(photo_post.id).to eq data["id"]
+    end
+
+    it "#imageが最大の画像ファイルを返す" do
+      got_image_file = photo.image
+      expect(got_image_file).to be_a_kind_of File
+      expected_image = File.binread('./file/photo_post.json')
+      expect(got_image_file.read).to eq expected_image
+    end
+
+  end
+
+end
