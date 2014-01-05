88ff57733bbb58c54d4655247bd6ea1f4af5ff6a
ri <git@tnu.jp>
2014-01-06 00:01:31 +0900

    保存関係の構造を見直したり、インターフェースを作ったりした

--- lib/save_store.rb
+++ lib/save_store.rb
@@ -1,62 +1,69 @@
 # encoding:utf-8
 require 'open-uri'
+require_relative './loader'
 
 class SaveFailedError < RuntimeError ; end
 
+
+
 class SaveStore
-  def initialize(save_dir)
-    @save_dir =  save_dir
-    @save_dir.concat('/') unless @save_dir.end_with?('/')
-    FileUtils.mkdir_p("#{@save_dir}json/")
-    FileUtils.mkdir_p("#{@save_dir}photo/")
+  Save_each_obj_type = [:photo,:video,:audio]
+  class SaveDir
+    def initialize(root)
+      @root_dir = File.expand_path(root)
+
+    end
+
+    def for_data(type)
+      if Save_each_obj_type.include? type
+        "#{@root_dir}/#{type}/#{type}"
+      else 
+        nil
+      end
+    end
+    
+    def for_json(type)
+      "#{@root_dir}/#{type}/json"
+    end
   end
+
+  def initialize(save_root_dir)
+    @save_root_dir =  File.expand_path(save_root_dir)
+    @save_root_dir.concat('/') unless @save_root_dir.end_with?('/')
+  end
+
+  def create_save_root_dirs
+    SaveStore.TYPES.each do |type|
+      root_for_type = "#{@save_root_dir}#{type}/"
+      if Save_each_obj_type.include? type
+        FileUtils.mkdir_p("#{root_for_type}#{type}/")
+      end
+      FileUtils.mkdir_p("#{root_for_type}json/")
+    end
+  end
+
+  def save(post)
+    case post
+    when PhotoPost
+      save_photo(post)
+    end
+  end
+
+
+  private
 
   def save_json(hash,filename_without_extension)
     json = JSON.generate(hash)
-    File.write("#{@save_dir}json/#{filename_without_extension}.json",json)
+    File.write("#{@save_root_dir}json/#{filename_without_extension}.json",json)
   end
 
   def save_quote(hash)
 
   end
 
-  def save_photo(image_urls,filename_without_extension)
-    min_img = []
-    image_data = image_urls.lazy.map {|url|
-      result = try_open_url(url)
-      next false unless result
-      if result.size < 1024*2 # less than 2KB
-        min_img << [url,result]
-        next false
-      end
-      next [url,result]
-    }.first
+  def save_photo(post)
 
-    if !image_data
-      min = min_img.shift
-      if min
-        image_data = min
-      else
-        raise SaveFailedError
-      end
-    end
-
-
-
-    image = image_data[1].read
-    extension = File.extname(image_data[0])
-
-    File.binwrite("#{@save_dir}photo/#{filename_without_extension}#{extension}",image)
-
-    nil
   end
-
-  def try_open_url(url)
-    open(url)
-  rescue
-    return false
-  end
-
-
 
 end
+
