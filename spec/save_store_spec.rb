a42b5b7820c8126f84e68b76a5976a3372cd9870
ri <git@tnu.jp>
2014-01-28 14:45:24 +0900

    fix spec

--- spec/save_store_spec.rb
+++ spec/save_store_spec.rb
@@ -16,16 +16,16 @@ describe SaveStore do
     end
 
     it "画像を保存できている" do
-      test_photo_path = "#{@tmpdir}/photo/#{@photo_post.id}.png"
+      test_photo_path = "#{@tmpdir}/photo/#{@photo_post.id}#{@photo_post.extension}"
       test_photo = File.binread(test_photo_path)
-      prepared_photo = File.binread('./file/lenna.png')
-      expect(test_photo).to eq prepared_photo
+      prepared_photo = File.binread('./file/photo_post.jpg')
+      expect(test_photo.size).to eq prepared_photo.size
     end
 
     it "jsonを保存できている" do
       test_json_path =  "#{@tmpdir}/json/#{@photo_post.id}.json"
       stored_json_file = File.read(test_json_path)
-      the_data = JSON.load(stored_json)
+      the_data = JSON.load(stored_json_file)
       expect(the_data).to eq @data
     end
 
