07b287c70754a6df2471d4e1cf7fbec78f9c506e
ri <git@tnu.jp>
2014-03-07 20:31:42 +0900

    ref spec

--- spec/resumable_tumblelog_spec.rb
+++ spec/resumable_tumblelog_spec.rb
@@ -12,29 +12,30 @@ describe ResumableTumblelog do
   end
 
   context "a" do
-    before :each do
+    before :all do
       @original = Tumblelog.new("staff",@config)
-      @resumable = ResumableTumblelog.new("staff",@config)
     end
 
     it "get 21 no resume" do
+      resumable = ResumableTumblelog.new("staff",@config)
+
       orig = @original.each_post.lazy.map(&:id).take(21).to_a
-      resm = @resumable.each_post.lazy.map(&:id).take(21).to_a
+      resm = resumable.each_post.lazy.map(&:id).take(21).to_a
       expect(resm).to eq orig
     end
 
     it "get3, resume, get3" do
+      resumable = ResumableTumblelog.new("staff",@config)
       orig = @original.each_post.lazy.map(&:id).take(6).to_a
 
       resm = []
-      resm.concat @resumable.each_post.lazy.map(&:id).take(3).to_a
-      state = @resumable.save_state
+      resm.concat resumable.each_post.lazy.map(&:id).take(3).to_a
+      state = resumable.save_state
       resumable_2 = ResumableTumblelog.restore(state,@config)
       resm.concat resumable_2.each_post.lazy.map(&:id).take(3).to_a
-      
+
       expect(resm).to eq orig
     end
-
   end
 end
 
